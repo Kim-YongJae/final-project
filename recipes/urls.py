9877679dc0d9124e4641386a9ba55246613e3ec1
YongJae Kim <golddragon939@gmail.com>
2023-11-27 17:15:09 +0900

    add YOLO

--- recipes/urls.py
+++ recipes/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-
 from recipes import views
 
 urlpatterns = [
-    path("R_index/", views.R_index, name="R_index"),
+    path('recipes/recommend/', views.detect_ingredients, name='detect_ingredients'),
 ]
